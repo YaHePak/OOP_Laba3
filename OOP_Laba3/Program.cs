6c7bff6af00eb43cb02714b7019a93772b5695c7
Айдар Шайдуллин <aydar4uk123123@mail.ru>
2020-11-06 17:32:22 +0500

    Добавьте файлы проекта.

---
+++ OOP_Laba3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
