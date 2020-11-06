44643950ab03d552426df9036bccade1354aacc8
Айдар Шайдуллин <aydar4uk123123@mail.ru>
2020-11-06 18:10:52 +0500

    Создан класс Item

---
+++ OOP_Laba3/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Laba3
+{
+    public class Item <A>
+    {
+        private T data = default(A); // Значение по умолчанию
+    }
+}
